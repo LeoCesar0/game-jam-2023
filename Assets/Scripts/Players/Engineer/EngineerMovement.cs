d4c70a197c25e66bca40667c33a36c3ee0250589
izaelle <izaelleavrillavigne@gmail.com>
2023-08-26 16:47:50 -0300

    commit

--- Assets/Scripts/Players/Engineer/EngineerMovement.cs
+++ Assets/Scripts/Players/Engineer/EngineerMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EngineerMovement : MonoBehaviour
@@ -7,16 +9,35 @@ public class EngineerMovement : MonoBehaviour
     private Rigidbody2D rb;
     public float speed = 5;
     public bool lookingRight = true;
+    private Animator animator;
+    // private Vector3 facingRight;
+    //  private Vector3 facingLeft;
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        // facingRight = transform.localScale;
+        // facingLeft = transform.localScale;
+        // facingLeft.x = facingLeft.x * -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // if(direction > 0)
+        // {
+        //     //olhando para direita
+        //     transform.localScale = facingRight;
+        // }
+        // if(direction < 0)
+        // {
+        //     transform.localScale = facingLeft;
+        //     //olhando para esquerda
+        // }
+       
+       
         float hAxis = 0;
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -29,6 +50,17 @@ public class EngineerMovement : MonoBehaviour
             hAxis = 1;
             lookingRight = true;
         }
+         if(hAxis != 0 )
+        {
+           //esta correndo
+           animator.SetBool("Run", true);
+        }
+        else 
+        {
+            animator.SetBool("Run", false);
+            //esta parado
+        }
+
         float velocityX = hAxis * speed * Time.deltaTime * 100;
         rb.velocity = new Vector2(velocityX, rb.velocity.y);
     }
